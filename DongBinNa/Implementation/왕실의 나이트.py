b827f87d58a451fbdd9d0aad3f780981b6841abc
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-10-10 19:19:20 +0900

    나동빈 - 왕실의 나이트

--- DongBinNa/Implementation/왕실의 나이트.py
+++ DongBinNa/Implementation/왕실의 나이트.py
@@ -1,4 +1,16 @@
-data = input()
-col = int(data[1])
-row = ord(data[0])  # a 97 ~ h 104
-h, w = 8
+input_data = input()
+# column = 가로 영어
+column = int(ord(input_data[0])) - int(ord('a')) + 1
+# row = 세로 숫자
+row = int(input_data[1])
+
+steps = [(-2, 1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+cnt = 0
+for step in steps:
+
+    nr = row + step[0]
+    nc = column + step[1]
+    if nr >= 1 and nr <= 8 and nc >= 1 and nc <= 8:
+        cnt += 1
+
+print(cnt)
